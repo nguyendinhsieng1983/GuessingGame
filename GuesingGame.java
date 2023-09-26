f7065f4e8a4f91e9b5507c25da4ddd85ecb32dca
Sieng <nguyendinhsieng1983@gmail.com>
2023-09-26 17:36:46 +0300

    init source

---
+++ GuesingGame.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class GuesingGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner inpt = new Scanner(System.in);
+		
+		
+		
+	}
+
+}
