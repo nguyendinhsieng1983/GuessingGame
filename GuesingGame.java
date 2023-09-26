fa5d480fcaa05f59346daeb7ac1102175564d443
Sieng <nguyendinhsieng1983@gmail.com>
2023-09-26 17:37:33 +0300

    doing scanner and println

--- GuesingGame.java
+++ GuesingGame.java
@@ -6,6 +6,19 @@ public class GuesingGame {
 		// TODO Auto-generated method stub
 		Scanner inpt = new Scanner(System.in);
 		
+		String strName;
+		int counter = 1;
+
+		System.out.println("Please, guess my name.");
+		strName = inpt.nextLine();
+		
+		
+		
+
+		
+		System.out.println(strName);
+		System.out.println("Congratulation!");
+		System.out.println("Guesses: " + counter);
 		
 		
 	}
